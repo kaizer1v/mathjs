69603ba61a8162ca250ffa5be0c6bd309ac8f3f6
kaizer1v <kaizer1v@gmail.com>
2015-03-20 19:14:15 +0530

    comments, description of functions

--- math.js
+++ math.js
@@ -7,10 +7,7 @@ function isArrayLike(obj) {
 		obj.length >= 0 &&
 		obj.length == Math.floor(obj.length) &&
 		obj.length < Math.pow(2, 32)
-	)
-	? true
-	: false
-	;
+	) ? true : false;
 }
 
 
@@ -26,9 +23,9 @@ var MJ = (function() {
 		* =======================
 		*/
 		function _extendClass(fromClass) {
-			if(typeof fromClass == "funciton" || (fromClass.constructor && fromClass.constructor === Function)) {
-				function Obj() {};
-				Obj.prototype = new fromClass;
+			if(typeof fromClass == "function" || (fromClass.constructor && fromClass.constructor === Function)) {
+				function Obj() {}
+				Obj.prototype = new fromClass();
 				return Obj;
 			}
 		}
@@ -37,16 +34,16 @@ var MJ = (function() {
 		var _on = (function() {
 			return function(obj, event, fn) {
 				return obj.addEventListener(event, fn);
-			}
+			};
 		})();
 
 
 
 
 		/* =======================
-		 * NUMBER FUNCTIONS
-		 * =======================
-		 */
+		* NUMBER FUNCTIONS
+		* =======================
+		*/
 		/* returns factorials of the number. You can use it like so: */
 		function _isPrime(n) {
 			var factors = _factors(n);
@@ -70,9 +67,10 @@ var MJ = (function() {
 
 		function _absolute(n) {
 			if(n.constructor === Number && !n.hasOwnProperty("square")) {
-				if(n > 0) var v = n;
-				else if(n < 0) var v = -1*(n);
-				else if(n == 0) var n = 1;
+				var v;
+				if(n > 0) v = n;
+				else if(n < 0) v = -1*(n);
+				else if(n === 0) n = 1;
 				return v;
 			} else throw TypeErorr("Type has to be a number only");
 		}
@@ -85,9 +83,10 @@ var MJ = (function() {
 		function _factors(n) {
 			var factors = [1, n];
 			for(var i = 2; i <= Math.floor(n/i); i++) {
-				if(_remainder(n, i) === 0 && (n % i == 0))
-					if(i === n/i)
+				if(n % i === 0)
+					if(i === n/i) {
 						factors.push(i);
+					}
 					else
 						factors.push(i, n/i);
 			}
@@ -96,50 +95,60 @@ var MJ = (function() {
 
 		var _sqrt = (function() {
 			// private
-			const tolerance = 0.0001;
+			var tolerance = 0.0001;
 
 			function improve(guess, x) {
 				return average(guess, (x / guess));
 			}
 			function average(x, y) {
-				return ((x + y) / 2)
+				return ((x + y) / 2);
 			}
 			function goodEnough(guess, x) {
-				return (abs(square(guess) - x) < tolerance) ? true : false
+				return (abs(square(guess) - x) < tolerance) ? true : false;
 			}
 			function square(x) {
 				return x*x;
 			}
 			function abs(x) {
+				var v;
 				if(x > 0)
-					var v = x;
-					else if(x < 0)
-						var v = -1*(x);
-						else if(x == 0)
-							var v = 1;
-							return v;
+					v = x;
+				else if(x < 0)
+					v = -1*(x);
+				else if(x === 0)
+					v = 1;
+					return v;
+			}
+			function isPerfectSq(n) {
+				var i;
+				for(i = 1; i <= Math.floor(n/i); i++) {
+					if(n % i === 0)
+						if(i === n/i)
+							return true;
+				}
+				return false;
 			}
 
 			// public
 			return function sqrt(n, guess) {
-				var guess = typeof guess !== "undefined" ? guess : 1,
-						z = (n / guess),
+				guess = typeof guess !== "undefined" ? guess : 1;
+				var z = (n / guess),
 						m = average(z, guess)
 				;
 				return (goodEnough(m, n)) ? m : sqrt(n, m);
-			}
+			};
 		})();
 
 		/* =======================
-		 * ARRAY FUNCTIONS
-		 * =======================
-		 */
+		* ARRAY FUNCTIONS
+		* =======================
+		*/
 
 		/* Takes a multi-dimensional array as argument and flattens it to a single dimensional array */
 		function _arrayFlatten(arr) {
 			if(arr && arr.constructor === Array) {
 				return arr.reduce(function(acc, x) {
-					return acc.concat(x && x.constructor === Array ? _flattenArray(x) : x);
+					return acc.concat(x && x.constructor === Array ? _arrayFlatten(x) : x);
 				}, []);
 			} else {
 				throw TypeError("Type should be an array only");
@@ -160,11 +169,12 @@ var MJ = (function() {
 
 		/* a - b = returns elements that exist a but not in b. */
 		function _arraySubstract(arr1, arr2) {
+			var biggerArr, smallerArr;
 			if((arr1 && arr2) && (arr1.constructor === Array && arr2.constructor === Array)) {
 				if(arr1 >= arr2) {
-					var biggerArr = arr1, smallerArr = arr2;
+					biggerArr = arr1, smallerArr = arr2;
 				} else {
-					var biggerArr = arr2, smallerArr = arr1;
+					biggerArr = arr2, smallerArr = arr1;
 				}
 
 				for(var i = 0; i != biggerArr.length; i++) {
@@ -182,10 +192,11 @@ var MJ = (function() {
 
 		/* Merges two arrays and removes all duplicates. */
 		function _arrayMerge() {
+			var currArr;
 			if(arguments && arguments.length == 1) {
 				return arguments[0];
 			} else if(arguments && arguments.length > 1) {
-				var currArr = arguments[0];
+				currArr = arguments[0];
 				for(var i = 0; i != arguments.length; i++) {
 					if(arguments[i].constructor === Array) {
 						for(var j = 0; j != arguments[i].length; j++) {
@@ -214,12 +225,12 @@ var MJ = (function() {
 		/* returns union along with duplicates of arrays passed in the parameters */
 		function _arrayUnion() {
 			if((arguments && arguments.length == 1) || !arguments[1]) return arguments[0];
-			if(arguments && arguments.length == 0) throw Error("You need to pass atleast 1 argument");
-			var tempArr = arguments[0];
-			for(var i = 1; i != arguments.length; i++) {
-				arguments[i].forEach(function(elem) {
-					tempArr.push(elem);
-				});
+			if(arguments && arguments.length === 0) throw Error("You need to pass atleast 1 argument");
+			var tempArr = arguments[0], i, j;
+			for(i = 1; i != arguments.length; i++) {
+				for(j = 0; j != arguments[i].length; j++) {
+					tempArr.push(arguments[i][j]);
+				}
 				if(arguments[i+1]) _arrayUnion(tempArr, arguments[i+1]);
 				return tempArr;
 			}
@@ -242,8 +253,9 @@ var MJ = (function() {
 
 		/* returns an array of all duplicates existing within the array */
 		function _arrayGetDuplicates(arr) {
+			var toReturn;
 			if(arr.constructor === Array) {
-				var toReturn = [];
+				toReturn = [];
 				for(var i = 0; i != arr.length; i++) {
 					if(_arrayHasDuplicates(arr, arr[i])) {
 						toReturn.push(arr[i]);
@@ -257,7 +269,7 @@ var MJ = (function() {
 
 		/* Sorts an array in a descending form */
 		function _arraySort(arr, ascDesc) {
-			var ascDesc = ascDesc || true;
+			ascDesc = ascDesc || true;
 			if(arr.constructor === Array) {
 				if(ascDesc) {						// ascending
 					arr.sort(function(a, b) {
@@ -280,6 +292,8 @@ var MJ = (function() {
 			return arr;
 		}
 
+		/* Statistics based */
+		/* returns mean of all numeric data in arr */
 		function _arrayMean(arr) {
 			if(arr.constructor === Array)
 				return (_arraySum(arr) / arr.length);
@@ -287,6 +301,7 @@ var MJ = (function() {
 				throw TypeError("Type should be an array only");
 		}
 
+		/* return the mode of numeric arr */
 		function _arrayMode(arr) {
 			if(arr.constructor === Array) {
 				var arrObj;
@@ -298,6 +313,7 @@ var MJ = (function() {
 				throw TypeError("Type should be an array only");
 		}
 
+		/* returns sum of all numbers in arr */
 		function _arraySum(arr) {
 			if(arr.constructor === Array)
 				return arr.reduce(function(a, b) { return a + b; }, 0);
@@ -305,6 +321,7 @@ var MJ = (function() {
 				throw TypeError("Type should be an array only");
 		}
 
+		/* returns product of all numbers in array */
 		function _arrayProduct(arr) {
 			if(arr.constructor === Array)
 				return arr.reduce(function(a, b) { return a * b; });
@@ -312,6 +329,7 @@ var MJ = (function() {
 				throw TypeError("Type should be an array only");
 		}
 
+		/* returns max element out of all numbers in arr */
 		function _arrayMax(arr) {
 			if(arr.constructor === Array)
 				return arr.reduce(function(a, b) { return (a > b) ? a : b; });
@@ -319,6 +337,7 @@ var MJ = (function() {
 				throw TypeError("Type should be an array only");
 		}
 
+		/* returns min element out of all numbers in arr */
 		function _arrayMin(arr) {
 			if(arr.constructor === Array)
 				return arr.reduce(function(a, b) { return (a < b) ? a : b; });
@@ -326,7 +345,8 @@ var MJ = (function() {
 				throw TypeError("Type should be an array only");
 		}
 
-		/* [a, b, c].pow() = ((a^b)^c) and so on. */
+		/* returns cumulative power of all numbers in arr like: */
+		/* _arrayPow([a, b, c]) = ((a^b)^c) and so on. */
 		function _arrayPow(arr) {
 			if(arr.constructor === Array)
 				return arr.reduce(function(a, b) { return Math.pow(a, b); });
@@ -342,13 +362,14 @@ var MJ = (function() {
 		/* This function takes an Object "obj", a key and a value as parameters and filters out if that key-value pair exists */
 		function _filterObject(obj, key, value) {
 			if(obj.constructor === Object) {
-				return obj.filter(function(x) { return x[key] == value; })
+				return obj.filter(function(x) { return x[key] == value; });
 			} else throw TypeError("Type should be an object only");
 		}
 
+		/* returns value of the 'key' in the 'obj' if exists */
 		function _getObjectVal(obj, key) {
 			if(obj.constructor === Object && obj[key]) {
-				return obj.filter(function(x) { return x[key]; })
+				return obj.filter(function(x) { return x[key]; });
 			} else throw TypeError("Type should be an object only");
 		}
 
@@ -383,8 +404,8 @@ var MJ = (function() {
 			// Object functionalities
 			objFilter: _filterObject,
 			objGetVal: _getObjectVal
-		}
-	};
+		};
+	}
 
 	return {
 		// creating a new singleton instance.
