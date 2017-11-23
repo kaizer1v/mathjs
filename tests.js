6745736d77e8a4ed2e850daad3dc5235faa52b35
kaizer1v <kaizer1v@gmail.com>
2017-11-23 19:03:47 +0530

    passed for array functions

--- tests.js
+++ tests.js
@@ -107,3 +107,9 @@ test2(MJ.arraySort([false, 'a', true, 1, 0]), [false, 'a', true, 0, 1])
 test2(MJ.arraySort([]), [])
 test2(MJ.arraySort(['z', 'g', 'b', 'm']), ['b', 'g', 'm', 'z'])   // sorts strings also
 test2(MJ.arraySort([undefined, NaN, false, 1, true, 0]), [0, NaN, false, 1, true, undefined])
+
+
+console.log('-------------------', 'arraySort');
+test(MJ.arrayMean, [1, 2, 3, 100, 101, 202], 68.16666666666667)
+test(MJ.arrayMean, [1.412, 2.53, 3*5.01, 100/142], 4.919056338028168)
+test(MJ.arrayMean, [false, 'abc'], NaN)
